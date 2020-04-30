0d95e832bb0a692d81e991258de7e540c675faf8
abhinav jain <jainabhinav2k16@gmail.com>
2020-04-30 13:46:10 +0530

    Binary Search Trees

---
+++ TrainingSet6/task 1/srchTree.h
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+  int key;
+  int height;
+  int depth;
+  struct node* left;
+  struct node* right;
+};
+void initTree();
+void insertKey(int key);
+void deleteKey(int key);
+int hasKey(int key);
+void setDepths();
+void setHeights();
+void printTree();
